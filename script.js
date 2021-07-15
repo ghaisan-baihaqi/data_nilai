21c87ba06ee8a41b71d1cbcd1883a5caddf79a7d
ghaisan-baihaqi <78635298+ghaisan-baihaqi@users.noreply.github.com>
2021-07-15 16:42:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+const soal = document.getElementsByClassName('jumlah-soal')
+const rata = document.getElementsByClassName('rata-rata');
+
+//bahasa indonesia
+let bIndo = 0;
+let nilai_bIndo = []
+let rata_bIndo = 0;
+for(i = 0; i < nilai_bIndo.length; i++){
+   rata_bIndo += nilai_bIndo[i]
+}
+soal[bIndo].innerHTML = nilai_bIndo.length
+rata[bIndo].innerHTML = rata_bIndo/nilai_bIndo.length
+
+//matematika
+let mtk = 1;
+let nilai_mtk = []
+let rata_mtk = 0;
+for(i = 0; i < nilai_mtk.length; i++){
+   rata_mtk += nilai_mtk[i]
+}
+soal[mtk].innerHTML = nilai_mtk.length
+rata[mtk].innerHTML = rata_mtk/nilai_mtk.length
+
+//ilmu pengetahuan alam
+let ipa = 2;
+let nilai_ipa = []
+let rata_ipa = 0;
+for(i = 0; i < nilai_ipa.length; i++){
+   rata_ipa += nilai_ipa[i]
+}
+soal[ipa].innerHTML = nilai_ipa.length
+rata[ipa].innerHTML = rata_ipa/nilai_ipa.length
+
+
+//ilmu pengetahuan sosial
+let ips = 3;
+let nilai_ips = []
+let rata_ips = 0;
+for(i = 0; i < nilai_ips.length; i++){
+   rata_ips += nilai_ips[i]
+}
+soal[ips].innerHTML = nilai_ips.length
+rata[ips].innerHTML = rata_ips/nilai_ips.length
+
+//pendidikan agama islam
+let pai = 4;
+let nilai_pai = []
+let rata_pai = 0;
+for(i = 0; i < nilai_pai.length; i++){
+   rata_pai += nilai_pai[i]
+}
+soal[pai].innerHTML = nilai_pai.length
+rata[pai].innerHTML = rata_pai/nilai_pai.length
+
+//pendidikan jasmani, olahraga, dan kesehatan
+let pjok = 5;
+let nilai_pjok = []
+let rata_pjok = 0;
+for(i = 0; i < nilai_pjok.length; i++){
+   rata_pjok += nilai_pjok[i]
+}
+soal[pjok].innerHTML = nilai_pjok.length
+rata[pjok].innerHTML = rata_pjok/nilai_pjok.length
+
+//prakarya
+let prakarya = 6;
+let nilai_prakarya = []
+let rata_prakarya = 0;
+for(i = 0; i < nilai_prakarya.length; i++){
+   rata_prakarya += nilai_prakarya[i]
+}
+soal[prakarya].innerHTML = nilai_prakarya.length
+rata[prakarya].innerHTML = rata_prakarya/nilai_prakarya.length
+
+//pendidikan pancasila dan kewarganegaraan
+let ppkn = 7;
+let nilai_ppkn = []
+let rata_ppkn = 0;
+for(i = 0; i < nilai_ppkn.length; i++){
+   rata_ppkn += nilai_ppkn[i]
+}
+soal[ppkn].innerHTML = nilai_ppkn.length
+rata[ppkn].innerHTML = rata_ppkn/nilai_ppkn.length
+
+//seni budaya
+let sb = 8;
+let nilai_sb = []
+let rata_sb = 0;
+for(i = 0; i < nilai_sb.length; i++){
+   rata_sb += nilai_sb[i]
+}
+soal[sb].innerHTML = nilai_sb.length
+rata[sb].innerHTML = rata_sb/nilai_sb.length
+
+//bahasa inggris
+let bInggris = 9;
+let nilai_bInggris = []
+let rata_bInggris = 0;
+for(i = 0; i < nilai_bInggris.length; i++){
+   rata_bInggris += nilai_bInggris[i]
+}
+soal[bInggris].innerHTML = nilai_bInggris.length
+rata[bInggris].innerHTML = rata_bInggris/nilai_bInggris.length
+
+//bahasa jawa
+let bJawa = 10;
+let nilai_bJawa = []
+let rata_bJawa = 0;
+for(i = 0; i < nilai_bJawa.length; i++){
+   rata_bJawanilai_bJawa += nilai_bJawa[i]
+}
+soal[bJawa].innerHTML = nilai_bJawa.length
+rata[bJawa].innerHTML = rata_bJawa/nilai_bJawa.length
